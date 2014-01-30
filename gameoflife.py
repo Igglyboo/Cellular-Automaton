ab067c6acda04abb103234a8480a467a9640d106
Trace Pillars <trace.pillars@e7solutions.com>
2014-01-29 19:16:25 -0500

    code cleanup

--- gameoflife.py
+++ gameoflife.py
@@ -1,19 +1,17 @@
 import pyglet
 import numpy
+from itertools import cycle
 from pyglet.window import key, mouse
 
-window_width = 1200
-window_height = 600
+window_width = 1000
+window_height = 700
 
-cell_size = 25
+cell_size = 100
 cells_high = window_height / cell_size
 cells_wide = window_width / cell_size
 
-grid = numpy.ones(dtype=int, shape=(cells_wide, cells_high))
+grid = numpy.zeros(dtype=int, shape=(cells_wide, cells_high))
 working_grid = numpy.zeros(dtype=int, shape=(cells_wide, cells_high))
-
-dead_color = [1.0, 1.0, 1.0, 1.0]
-alive_color = [0.0, 0.0, 0.0, 1.0]
 
 born = {3}
 survives = {2, 3}
@@ -26,32 +24,29 @@ window.set_caption("Cellular Automaton")
 
 @window.event
 def on_draw():
-    global alive_color
     window.clear()
     color_cells()
     draw_grid()
-    alive_color = color.next()
 
 
 @window.event
 def on_key_press(symbol, modifiers):
     global paused
-    global grid
     if symbol == key.ENTER:
         paused = not paused
-    elif symbol == key.I and paused:
-        grid.fill(0)
-    elif symbol == key.O and paused:
-        grid.fill(1)
-    elif symbol == key.P and paused:
-        randomize_grid()
-    elif symbol == key.RIGHT and paused:
-        update_grid()
+    elif paused:
+        if symbol == key.I:
+            grid.fill(0)
+        elif symbol == key.O:
+            grid.fill(1)
+        elif symbol == key.P:
+            randomize_grid()
+        elif symbol == key.RIGHT:
+            update_grid()
 
 
 @window.event
 def on_mouse_press(x, y, button, modifiers):
-    global grid
     if button == mouse.LEFT and paused:
         grid[x/cell_size][y/cell_size] = not grid[x/cell_size][y/cell_size]
 
@@ -62,7 +57,7 @@ def update(dt):
 
 
 def draw_grid():
-    pyglet.gl.glColor4f(dead_color[0], dead_color[1], dead_color[2], dead_color[3])
+    pyglet.gl.glColor4f(1.0, 1.0, 1.0, 1.0)
     for i in range(0, window_width, cell_size):
         pyglet.graphics.draw(2, pyglet.gl.GL_LINES, ('v2i', (i, 0, i, window_height)))
     for i in range(0, window_height, cell_size):
@@ -70,6 +65,7 @@ def draw_grid():
 
 
 def color_cells():
+    alive_color = color_iterator.next()
     pyglet.gl.glColor4f(alive_color[0], alive_color[1], alive_color[2], alive_color[3])
     for x in xrange(cells_wide):
         for y in xrange(cells_high):
@@ -103,50 +99,38 @@ def get_neighbors(x, y):
     n = 0
     for i in range(-1, 2):
         for j in range(-1, 2):
-            if not(i == 0 and j == 0):
+            if i or j:
                 if 0 <= x + i < cells_wide and 0 <= y + j < cells_high:
-                    if grid[x + i][y + j] == 1:
-                        n += 1
+                    n += grid[x + i][y + j]
     return n
 
 
 def randomize_grid():
-    global grid
     for x in xrange(cells_wide):
         for y in xrange(cells_high):
             grid[x][y] = numpy.random.randint(0, 2)
 
 
 def color_generator():
-    while True:
-        r = 1.0
-        g = 0.0
-        b = 0.0
-        increment = 50
+    color = [1.0, 0, 0]
+    iterations = 50
+    increment = 1.0 / iterations
+    fill = True
 
-        for i in xrange(increment):
-            g += 1.0/increment
-            yield [r, g, b, 1.0]
-        for i in xrange(increment):
-            r -= 1.0/increment
-            yield [r, g, b, 1.0]
-        for i in xrange(increment):
-            b += 1.0/increment
-            yield [r, g, b, 1.0]
-        for i in xrange(increment):
-            g -= 1.0/increment
-            yield [r, g, b, 1.0]
-        for i in xrange(increment):
-            r += 1.0/increment
-            yield [r, g, b, 1.0]
-        for i in xrange(increment):
-            b -= 1.0/increment
-            yield [r, g, b, 1.0]
+    for i in cycle((1, 0, 2)):
+        for n in xrange(iterations):
+            if fill:
+                color[i] += increment
+            else:
+                color[i] -= increment
+            color.append(1.0);
+            yield color
+        fill = not fill
 
 
 if __name__ == "__main__":
     randomize_grid()
-    color = color_generator()
+    color_iterator = color_generator()
     pyglet.clock.schedule_interval(update, 1.0/15.0)
     pyglet.app.run()
 else:
