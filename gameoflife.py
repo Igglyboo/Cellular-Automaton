d979380fdd3b562701b5333234d3e15fe896c5c0
Trace Pillars <trace.pillars@e7solutions.com>
2014-02-01 01:32:25 -0500

    Cleaned up the code, sped up get_neighbors by unrolling the loop, removed the overkill numpy stuff

--- gameoflife.py
+++ gameoflife.py
@@ -1,24 +1,27 @@
-import pyglet
-import numpy
+from copy import deepcopy
+from random import randint
 from itertools import cycle
-from pyglet.window import key, mouse
 
-window_width = 700
-window_height = 700
+import pyglet
+from pyglet.window import Window, key, mouse
+
+window_width = 640
+window_height = 480
 
 cell_size = 10
 cells_high = window_height / cell_size
 cells_wide = window_width / cell_size
 
-grid = numpy.zeros(dtype=int, shape=(cells_wide, cells_high))
-working_grid = numpy.zeros(dtype=int, shape=(cells_wide, cells_high))
+grid = [1 for cell in range(cells_high)]
+grid = [grid[:] for cell in range(cells_wide)]
+working_grid = deepcopy(grid)
 
 born = {3}
-survives = {2, 3}
+survives = {2, 3, 8}
 
 paused = False
 
-window = pyglet.window.Window(window_width, window_height)
+window = Window(window_width, window_height)
 window.set_caption("Cellular Automaton")
 
 
@@ -36,9 +39,13 @@ def on_key_press(symbol, modifiers):
         paused = not paused
     elif paused:
         if symbol == key.I:
-            grid.fill(0)
+            for x in xrange(cells_wide):
+                for y in xrange(cells_high):
+                    grid[x][y] = 0
         elif symbol == key.O:
-            grid.fill(1)
+            for x in xrange(cells_wide):
+                for y in xrange(cells_high):
+                    grid[x][y] = 1
         elif symbol == key.P:
             randomize_grid()
         elif symbol == key.RIGHT:
@@ -49,19 +56,19 @@ def on_key_press(symbol, modifiers):
 def on_mouse_press(x, y, button, modifiers):
     if paused:
         if button == mouse.LEFT:
-            grid[x/cell_size][y/cell_size] = 1
+            grid[x / cell_size][y / cell_size] = 1
         elif button == mouse.RIGHT:
-            grid[x/cell_size][y/cell_size] = 0
-            
+            grid[x / cell_size][y / cell_size] = 0
+
 
 @window.event
 def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
     if paused:
         if 0 <= x / cell_size < cells_wide and 0 <= y / cell_size < cells_high:
             if buttons == mouse.LEFT:
-                grid[x/cell_size][y/cell_size] = 1
+                grid[x / cell_size][y / cell_size] = 1
             elif buttons == mouse.RIGHT:
-                grid[x/cell_size][y/cell_size] = 0
+                grid[x / cell_size][y / cell_size] = 0
 
 
 def update(dt):
@@ -70,16 +77,19 @@ def update(dt):
 
 
 def draw_grid():
-    pyglet.gl.glColor4f(1.0, 1.0, 1.0, 1.0)
+    batch = pyglet.graphics.Batch()
+    pyglet.gl.glColor3f(1.0, 1.0, 1.0)
     for i in range(0, window_width, cell_size):
-        pyglet.graphics.draw(2, pyglet.gl.GL_LINES, ('v2i', (i, 0, i, window_height)))
+        batch.add(2, pyglet.gl.GL_LINES, None, ('v2i', (i, 0, i, window_height)))
     for i in range(0, window_height, cell_size):
-        pyglet.graphics.draw(2, pyglet.gl.GL_LINES, ('v2i', (0, i, window_width, i)))
+        batch.add(2, pyglet.gl.GL_LINES, None, ('v2i', (0, i, window_width, i)))
+    batch.draw()
 
 
 def color_cells():
-    alive_color = color_iterator.next()
-    pyglet.gl.glColor4f(alive_color[0], alive_color[1], alive_color[2], alive_color[3])
+    batch = pyglet.graphics.Batch()
+    cell_color = color_iterator.next()
+    pyglet.gl.glColor3f(cell_color[0], cell_color[1], cell_color[2])
     for x in xrange(cells_wide):
         for y in xrange(cells_high):
             if grid[x][y]:
@@ -87,7 +97,8 @@ def color_cells():
                 y1 = y * cell_size
                 x2 = x1 + cell_size
                 y2 = y1 + cell_size
-                pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2i', (x1, y1, x1, y2, x2, y2, x2, y1)))
+                batch.add(4, pyglet.gl.GL_QUADS, None, ('v2i', (x1, y1, x1, y2, x2, y2, x2, y1)))
+    batch.draw()
 
 
 def update_grid():
@@ -105,23 +116,34 @@ def update_grid():
                     working_grid[x][y] = 1
                 else:
                     working_grid[x][y] = 0
-    grid = numpy.copy(working_grid)
+    grid = deepcopy(working_grid)
 
 
 def get_neighbors(x, y):
     n = 0
-    for i in range(-1, 2):
-        for j in range(-1, 2):
-            if i or j:
-                if 0 <= x + i < cells_wide and 0 <= y + j < cells_high:
-                    n += grid[x + i][y + j]
+    if 0 <= x < cells_wide and 0 <= y - 1 < cells_high:
+                    n += grid[x][y - 1]
+    if 0 <= x < cells_wide and 0 <= y + 1 < cells_high:
+                    n += grid[x][y + 1]
+    if 0 <= x + 1 < cells_wide and 0 <= y - 1 < cells_high:
+                    n += grid[x + 1][y - 1]
+    if 0 <= x + 1 < cells_wide and 0 <= y < cells_high:
+                    n += grid[x + 1][y]
+    if 0 <= x + 1 < cells_wide and 0 <= y + 1 < cells_high:
+                    n += grid[x + 1][y + 1]
+    if 0 <= x - 1 < cells_wide and 0 <= y - 1 < cells_high:
+                    n += grid[x - 1][y - 1]
+    if 0 <= x - 1 < cells_wide and 0 <= y < cells_high:
+                    n += grid[x - 1][y]
+    if 0 <= x - 1 < cells_wide and 0 <= y + 1 < cells_high:
+                    n += grid[x - 1][y + 1]
     return n
 
 
 def randomize_grid():
     for x in xrange(cells_wide):
         for y in xrange(cells_high):
-            grid[x][y] = numpy.random.randint(0, 2)
+            grid[x][y] = randint(0, 2)
 
 
 def color_generator():
@@ -136,7 +158,6 @@ def color_generator():
                 color[i] += increment
             else:
                 color[i] -= increment
-            color.append(1.0);
             yield color
         fill = not fill
 
@@ -144,7 +165,7 @@ def color_generator():
 if __name__ == "__main__":
     randomize_grid()
     color_iterator = color_generator()
-    pyglet.clock.schedule_interval(update, 1.0/15.0)
+    pyglet.clock.schedule_interval(update, 1.0 / 30.0)
     pyglet.app.run()
 else:
     exit()
